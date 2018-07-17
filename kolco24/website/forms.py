9a10844b30128b08bc1e44696428f2707333c9e9
alff0x1f <alff0x1f@gmail.com>
2018-07-17 17:55:40 +0500

    add login page

---
+++ kolco24/website/forms.py
@@ -0,0 +1,48 @@
+from django import forms
+from django.core.exceptions import ObjectDoesNotExist
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+
+
+class LoginForm(forms.Form):
+    email = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control form-control-lg',
+                'placeholder': 'Введите email'}),
+        label='Адрес email:')
+
+    password = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={
+                'class': 'form-control form-control-lg',
+                'placeholder': 'Введите пароль'}),
+        label='Пароль:')
+
+    def clean(self):
+        user = self.authenticate_via_email()
+        if not user:
+            raise forms.ValidationError("Неправильный логин или пароль.")
+        else:
+            self.user = user
+        return self.cleaned_data
+
+    def authenticate_user(self):
+        return authenticate(
+            username=self.user.username,
+            password=self.cleaned_data['password'])
+
+    def authenticate_via_email(self):
+        """
+            Authenticate user using email.
+            Returns user object if authenticated else None
+        """
+        email = self.cleaned_data['email']
+        if email:
+            try:
+                user = User.objects.get(email__iexact=email)
+                if user.check_password(self.cleaned_data['password']):
+                    return user
+            except ObjectDoesNotExist:
+                pass
+        return None
--- kolco24/website/views.py
+++ kolco24/website/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth import login as auth_login
+from website.forms import LoginForm
+from django.http import HttpResponseRedirect
 
-# Create your views here.
+
 def index(request):
     # return HttpResponse("Hello, world!")
     return render(request, 'website/index.html')
+
+
+def login(request):
+    form = LoginForm(request.POST or None)
+    if request.method == 'POST' and form.is_valid():
+        user = form.authenticate_user()
+        auth_login(request, user)
+        return HttpResponseRedirect("/")
+
+    return render(request, 'website/login.html', {'form': form})
