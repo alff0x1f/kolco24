f406e54ff10d5971f290de036bf9d7ab432d33e9
alff0x1f <alff0x1f@gmail.com>
2018-07-26 13:39:27 +0500

    function set_ucount

--- kolco24/static/js/team.js
+++ kolco24/static/js/team.js
@@ -18,50 +18,61 @@ $( "#dist_header" ).click(function() {
     // alert( "Handler for .click() called." );
 });
 
-$("#add_member3").click(function(){
-    $("#member3").removeClass('d-none');
-    $("#add_member3").parent().addClass('d-none');
-    ucount = 3;
-});
-$("#add_member4").click(function(){
-    $("#member4").removeClass('d-none');
-    $("#add_member4").parent().addClass('d-none');
-    $("#del_member3").parent().addClass('d-none');
-    ucount = 4;
-});
-$("#add_member5").click(function(){
-    $("#member5").removeClass('d-none');
-    $("#add_member5").parent().addClass('d-none');
-    $("#del_member4").parent().addClass('d-none');
-    ucount = 5;
-});
-$("#add_member6").click(function(){
-    $("#member6").removeClass('d-none');
-    $("#add_member6").parent().addClass('d-none');
-    $("#del_member5").parent().addClass('d-none');
-    ucount = 6;
-});
-
-$("#del_member3").click(function(){
-    $("#member3").addClass('d-none');
-    $("#add_member3").parent().removeClass('d-none');
-    ucount = 2;
-});
-$("#del_member4").click(function(){
-    $("#member4").addClass('d-none');
-    $("#add_member4").parent().removeClass('d-none');
-    $("#del_member3").parent().removeClass('d-none');
-    ucount = 3;
-});
-$("#del_member5").click(function(){
-    $("#member5").addClass('d-none');
-    $("#add_member5").parent().removeClass('d-none');
-    $("#del_member4").parent().removeClass('d-none');
-    ucount = 4;
-});
-$("#del_member6").click(function(){
-    $("#member6").addClass('d-none');
-    $("#add_member6").parent().removeClass('d-none');
-    $("#del_member5").parent().removeClass('d-none');
-    ucount = 5;
-});
+function set_ucount(count) {
+    ucount = count;
+    $("#ucountlabel").text(count);
+    $("#sumlabel").text(count * cost);
+    // hide members:
+    if (count < 6) {
+        $("#member6").addClass('d-none');
+    } 
+    if (count < 5) {
+        $("#member5").addClass('d-none');
+    }
+    if (count < 4) {
+        $("#member4").addClass('d-none');
+    }
+    if (count < 3) {
+        $("#member3").addClass('d-none');
+    }
+    // show members and hide controls on top controls
+    if (count >= 3){
+        $("#member3").removeClass('d-none');
+        $("#add_member3").addClass('d-none');
+    }
+    if (count >= 4){
+        $("#member4").removeClass('d-none');
+        $("#add_member4").addClass('d-none');
+        $("#del_member3").addClass('d-none');
+    }
+    if (count >= 5){
+        $("#member5").removeClass('d-none');
+        $("#add_member5").addClass('d-none');
+        $("#del_member4").addClass('d-none');
+    }
+    if (count >= 6){
+        $("#member6").removeClass('d-none');
+        $("#add_member6").addClass('d-none');
+        $("#del_member5").addClass('d-none');
+    }
+    // show controls on last item
+    if (count == 2){
+        $("#del_member2").removeClass('d-none');
+        $("#add_member3").removeClass('d-none');
+    }
+    if (count == 3){
+        $("#del_member3").removeClass('d-none');
+        $("#add_member4").removeClass('d-none');
+    }
+    if (count == 4){
+        $("#del_member4").removeClass('d-none');
+        $("#add_member5").removeClass('d-none');
+    }
+    if (count == 5){
+        $("#del_member5").removeClass('d-none');
+        $("#add_member6").removeClass('d-none');
+    }
+    if (count == 6){
+        $("#del_member6").removeClass('d-none');
+    }
+  }
