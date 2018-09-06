6968a59f9769eb3dfc2ae8b5fa97df72f8663193
alff0x1f <alff0x1f@gmail.com>
2018-09-06 18:53:58 +0500

    change login email subject

--- kolco24/website/email.py
+++ kolco24/website/email.py
@@ -1,25 +1,9 @@
 from django.core.mail import send_mail, BadHeaderError
 from django.core.mail import EmailMessage
 
-def send_test_email(flogin_key):
-    subject = 'тема письма'
-    message = 'Ключ: %s' % flogin_key
-    from_email = "Кольцо 24 <org@kolco24.ru>"
-
-    recepient = []
-    recepient.append('alff3one@gmail.com')
-
-    email1 = EmailMessage(
-        subject,
-        message,
-        from_email,
-        recepient,
-        ["alff0x1f@gmail.com"]
-    )
-    email1.send(fail_silently=False)
 
 def send_login_email(recepient_email, flogin_key):
-    subject = 'Ссылка для входа'
+    subject = 'Кольцо24 - ссылка для входа'
     message = """Ссылка для входа:
 https://kolco24.ru/login/%s
 
