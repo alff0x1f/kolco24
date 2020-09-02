5dad17babf5c766b4d4da41961e97e980279d032
alff0x1f <alff0x1f@gmail.com>
2020-09-02 19:03:27 +0300

    fix

--- kolco24/website/admin.py
+++ kolco24/website/admin.py
@@ -25,8 +25,7 @@ class PaymentsYaAdmin(admin.ModelAdmin):
 
 
 class PaymentAdmin(admin.ModelAdmin):
-    list_display = ('id', 'payment_amount', 'cost_per_person', 'paid_for', 'status')
-    list_filter = ('amount')
+    list_display = ('id', 'team.teamname', 'team.id', 'payment_amount', 'cost_per_person', 'paid_for', 'status')
 
 admin.site.register(Team, TeamAdmin)
 admin.site.register(ControlPoint, ControlPointAdmin)
