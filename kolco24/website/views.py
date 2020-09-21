d89044c334d65ab5440a9c6460bf9f2b7b6ac213
ildus <alff0x1f@gmail.com>
2020-09-21 22:48:39 +0300

    Возможность зареганным командам оплачивать после закрытия регистрации

--- kolco24/website/views.py
+++ kolco24/website/views.py
@@ -312,7 +312,7 @@ def my_team(request, teamid="", template="my_team"):
             "main_team": main_team,
             "curr_time": datetime.now(timezone.utc) + timedelta(hours=5),
             "timestamp": time(),
-            'reg_open': int(teams_count) < 150,
+            'reg_open': int(teams_count) < 150 or main_team.paid_people > 0,
             'additional_charge': main_team.additional_charge,
         }
         if request.user.is_superuser:
