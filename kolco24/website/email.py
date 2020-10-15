828feae8f8356026c978f469c2d08948bdc05a59
alff0x1f <alff0x1f@gmail.com>
2020-10-15 23:39:58 +0300

    Рассылка на возврат взносов

--- kolco24/website/email.py
+++ kolco24/website/email.py
@@ -1,6 +1,6 @@
 from django.core.mail import send_mail, BadHeaderError
 from django.core.mail import EmailMessage
-from website.models import Team
+from website.models import Team, Payment, PaymentsYa
 import string
 import random
 
@@ -28,75 +28,36 @@ https://kolco24.ru/login/%s
     flogin_email.send(fail_silently=False)
 
 
-def send_message(recepient_email, start_number, teamname, password, username):
-    subject = 'Кольцо24 - Онлайн'
-    message = """Привет, {username}, в связи с тем, что в Челябинской области введен запрет на 
-любые массовые мероприятия, мы вынуждены отменить "Кольцо-24".
+def send_message(recepient_email, teamname, username, payments, return_sum):
+    subject = 'Возврат стартовых взносов за команду %s' % teamname
+    message = """Здравствуйте, {username}
 
-И объявить о проведении "Кольца-24 Онлайн"!
-Суть не меняется - вы можете так же сходить в поход выходного дня с картой и 
-собрать контрольные пункты, соблюдая все меры социального дистанцирования. 
-Всё остается прежним, кроме условий для массового скопления людей. Будем 
-считать, что вы уже выросли и достаточно самостоятельны для такого.
+Мы наконец-то отошли от оформления протоколов и готовы приступить к возврату взносов 
+тем командам, которые отказались от участия. 
+Ваша команда "{teamname}" в их числе. 
 
-Все подробности расскажем на сегодняшем (7 октября) онлайн-брифинге в 20:00 
-по уфимскому времени, ссылка на трансляцию будет в группе ВК.
+Платежи за команду:
+{payments}
+Сумма к возврату (с вычетом 8%): {return_sum} руб
 
-Старты 10 октября (суббота) в удобное время с 10.00 – 16.00.
+Варианты возврата следующие:
+ 1) Яндекс деньги (самый удобный для нас вариант, тк платежи поступали туда)
+ 2) Тиньков
+ 3) Сбербанк
+ 4) Карта любого банка с подключенной системой быстрых платежей (СБП)
+ 
+Если вы ещё не сообщали нам реквизиты и способ возврата, то это можно сделать, ответив 
+на это письмо. В случае Яндекс Денег необходим номер кошелька, в варианте банковской 
+карты, необходимо сообщить номер телефона, к которому привязана банковская карта.
 
-Ваша команда: 
-номер {start_number}, 
-Название {teamname}
-
-Необходимые действия перед выходом на маршрут:
-
-1) со стартовой точки сделать контрольный звонок по номеру (будет указан позже) 
-и сообщить о выходе на маршрут,
-2) включить запись трека устройстве,
-3) запустить приложение в смартфоне О-GPS (http://o-gps-center.ru)
-
-На дистанции - должна быть постоянная запись трека и запущенное приложение 
-O-GPS, на контрольных пунктах делаете фотографии со всеми участниками и ясно 
-различимым номером КП.
-
-При финишировании:
-1) Позвонить по указанному номеру с точки финиша.
-
-Трек, фото отправить на почту (будет указано позже) 12.10.20 до 23.00 по Уфе
-
-Обязательное снаряжение:
-1) Фотоаппарат с установленным и настроенным с точностью до минуты уфимским 
-временем (+5 GMT)
-2) Средство для записи трека (навигатор или смартфон)
-3) Смартфон с установленным приложением O-GPS (http://o-gps-center.ru, только 
-для андроид ) – для возможности онлайн наблюдения за командами.
-
-Вероятно, вам понадобиться дополнительный аккумулятор. Крайне не рекомендуем 
-объединять три вышеперечисленные устройства в одном смартфоне.
-Остальное рекомендуемое снаряжение не отличается от предыдущих лет.
-
-Как вы уже поняли – не будет никаких проверок обязательного снаряжения (но 
-лучше бы вам его иметь - горы не прощают ошибок), скопления людей в финишных 
-коридорах, и, к сожалению, борща.
-
-Но, если вы не хотите участвовать в таком формате, то можно сдать слоты и 
-получить назад деньги (за вычетом 8%, комиссии платежной системы), 
-либо перенести слоты на следующий год - в личном кабинете выбрать год участия 
-2021 год. Заявку на возврат взноса добавим в личный кабинет в скором времени.
-
-Реквизиты для входа в личный кабинет (https://kolco24.ru/passlogin):
-email: {email}
-пароль: {password}
-
-Все же надеемся, что никакие внешние силы не остановят вас в стремлении 
-насладиться особой природой Аджигардака.
+Спасибо за ожидание и терпение! Надеемся что в следующим году не будет таких 
+форм-мажорных факторов и соревнование пройдут в штатном режиме. 
 
 Команда организаторов Кольца 24
 """.format(username=username,
-           start_number=start_number,
            teamname=teamname,
-           email=recepient_email,
-           password=password)
+           payments=payments,
+           return_sum=return_sum)
 
     from_email = 'Кольцо 24 <org@kolco24.ru>'
 
@@ -113,18 +74,20 @@ email: {email}
     message.send(fail_silently=False)
 
 def send_to_all_teams():
-    teams = Team.objects.filter(year='2020').select_related('owner')
+    teams = Team.objects.filter(year='10').select_related('owner')
     teams = [t for t in teams if t.paid_sum > 0]
 
     for team in teams:
-        alphabet = string.ascii_letters + string.digits
-        password = ''.join(random.choice(alphabet) for _ in range(8))
-        team.owner.set_password(password)
-        team.owner.save()
-        print(team.teamname, team.owner.email, password)
+        payment_str = ''
+        payments = Payment.objects.filter(status='done', team_id=team.id)
+        for payment in payments:
+            ya_pay = PaymentsYa.objects.filter(label=payment.id)[0]
+            payment_str += '%s %s\n' % (ya_pay.datetime, ya_pay.withdraw_amount)
+            print(team.teamname, ' ', ya_pay.datetime, ya_pay.withdraw_amount)
         send_message(recepient_email=team.owner.email,
-                     start_number=team.start_number,
                      teamname=team.teamname,
-                     password=password,
-                     username=' '.join([team.owner.last_name,
-                                        team.owner.first_name]))
+                     username=team.owner.first_name,
+                     payments=payment_str,
+                     return_sum=team.paid_sum*0.92)
+
+
