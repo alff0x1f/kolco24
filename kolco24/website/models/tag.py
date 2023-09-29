76ea6baf95a41c2ada9a966ba85bcd3f54202029
alff0x1f <alff0x1f@gmail.com>
2023-09-29 18:22:19 +0500

    #11 fix 500 error

--- kolco24/website/models/tag.py
+++ kolco24/website/models/tag.py
@@ -5,7 +5,7 @@ class PointTag(models.Model):
     point = models.ForeignKey(
         "ControlPoint", verbose_name="КП", on_delete=models.CASCADE
     )
-    tag_id = models.CharField(max_length=255, unique=True)
+    tag_id = models.CharField(max_length=255)
 
     def __str__(self):
         return f"{self.id} - {self.tag_id}"
