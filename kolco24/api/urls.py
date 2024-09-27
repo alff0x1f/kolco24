3da855e9a770e30336481cb45feb990b361d984c
Ildus <alff0x1f@gmail.com>
2024-09-27 16:33:38 +0500

    control point tags (#57)

--- kolco24/api/urls.py
+++ kolco24/api/urls.py
@@ -1,4 +1,4 @@
-from api.views import MemberTagListCreateView
+from api.views import MemberTagListCreateView, PointTagsView
 from django.http import HttpResponse
 from django.urls import path
 
@@ -7,4 +7,9 @@ urlpatterns = [
         "ping/", lambda request: HttpResponse(content="[pong]", status=200), name="ping"
     ),
     path("member_tag/", MemberTagListCreateView.as_view(), name="tag-list-create"),
+    path(
+        "race/<int:race_id>/point_tags/",
+        PointTagsView.as_view(),
+        name="point_tags2",
+    ),
 ]
