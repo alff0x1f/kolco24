78bdd87ae9d7f222c196edef5b3186ef8c01bf13
alff0x1f <alff0x1f@gmail.com>
2024-10-04 16:04:11 +0500

    fix linter

--- src/api/urls.py
+++ src/api/urls.py
@@ -1,6 +1,7 @@
-from api.views import CheckpointTagCreateView, MemberTagListCreateView
 from django.http import HttpResponse
 from django.urls import path
+
+from api.views import CheckpointTagCreateView, MemberTagListCreateView
 
 urlpatterns = [
     path(
