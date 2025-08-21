eeb5f49655d8b0a1803025e7ed55d0a381b1d9fc
Ildus <alff0x1f@gmail.com>
2025-08-21 10:08:32 +0500

    feat: страницы с доп инфы (#80)

--- src/website/views/views_.py
+++ src/website/views/views_.py
@@ -1017,6 +1017,16 @@ def privacy_policy(request):
     return render(request, "website/privacy_policy.html")
 
 
+def refund_policy(request):
+    """Display refund policy page."""
+    return render(request, "website/refund_policy.html")
+
+
+def service_order_rules(request):
+    """Display service order rules page."""
+    return render(request, "website/service_order_rules.html")
+
+
 # API _________________________________________________________________
 class RaceView(View):
     def get(self, request):
