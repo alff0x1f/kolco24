390d46e32acb3fe3952c616d64fdd71a5096374f
alff0x1f <alff0x1f@gmail.com>
2025-09-14 19:53:02 +0500

    feat: implement footer menu using custom template tag

---
+++ src/website/templatetags/menu_tags.py
@@ -0,0 +1,9 @@
+from django import template
+from website.models import MenuItem
+
+register = template.Library()
+
+
+@register.inclusion_tag("website/_footer_menu.html")
+def footer_menu():
+    return {"menu": MenuItem.objects.all()}
